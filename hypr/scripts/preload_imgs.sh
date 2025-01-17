80398c61edb72b638dd0775d0390b8f6b48e03ba
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2025-01-17 20:04:51 +0100

    Add files via upload

---
+++ hypr/scripts/preload_imgs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/horizontal/1.png
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/horizontal/2.png
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/horizontal/3.jpg
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/horizontal/4.jpg
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/vertical/1.jpg
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/vertical/2.jpg
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/vertical/3.jpg
+hyprctl hyprpaper preload ~/.config/hypr/images/wallpapers/vertical/4.jpg
